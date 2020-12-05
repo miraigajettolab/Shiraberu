b709445c035a63381f1a72a7ca6fc8ab6b44dba1
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-12-05 21:06:42 +0500

    Add boilerplate client code

---
+++ shiraberu-client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          ↗️under↘️construction↗️peko↘️
+        </p>
+    </div>
+  );
+}
+
+export default App;
