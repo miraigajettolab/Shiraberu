ac74090352cd20a380d5ff6b67e32e2a7d0dca7f
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-12-08 04:22:39 +0500

    Make methods to access indices and create lesson queue

--- shiraberu-client/src/panels/home/Home.js
+++ shiraberu-client/src/panels/home/Home.js
@@ -7,13 +7,96 @@ class Home extends React.Component {
     constructor(props) {
     super(props)
         this.state = {
+            uid: " "
         }
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                this.setState({uid: user.uid})
+            } 
+        }.bind(this))
+
         this.signOutHandler = this.signOutHandler.bind(this)
+        this.getIndices = this.getIndices.bind(this)
+        this.countReviews = this.countReviews.bind(this)
+        this.countLessons = this.countLessons.bind(this)
     }
 
     signOutHandler() {
         firebase.auth().signOut()
         this.props.activePanelHandler("SignIn")
+    }
+
+    countReviews(){
+        var t0 = performance.now()
+        //TODO:
+        var t1 = performance.now()
+        console.log("Call to countReviews() took " + (t1 - t0) + " milliseconds.")
+    }
+
+    countLessons(){
+        var t0 = performance.now()
+        let lessonQueue = [];
+        Object.keys(this.state.itemsIdx).forEach(key => {
+            if(this.state.itemsIdx[key].srs === 0){
+                if(this.state.protsIdx[key].comp){ //if prototype has components
+                    let passed = true 
+                    this.state.protsIdx[key].comp.forEach(component => {
+                        if (this.state.itemsIdx[component].srs < 5){ //we check that every one of them passed
+                            passed = false
+                        }
+                    })
+                    if(passed){
+                        lessonQueue.push(key)
+                    }
+                }
+                else{
+                    lessonQueue.push(key)
+                }
+            }
+        })
+        this.setState({
+            "lessonQueue": lessonQueue
+        })
+        var t1 = performance.now()
+        console.log("Call to countLessons() took " + (t1 - t0) + " milliseconds.")
+    }
+
+    getIndices(){
+        var t0 = performance.now()
+        const db = firebase.firestore();
+        db.collection('Prototypes').doc('index').get()
+        .then(ProtsIndex => {
+                this.setState({
+                    protsIdx:ProtsIndex.data()
+                })
+                return 0
+            })
+            .then(check => {
+                db.collection('Users').doc(this.state.uid).collection('Items').doc('index').get()
+                .then(ItemsIndex => {
+                    this.setState({
+                        itemsIdx:ItemsIndex.data()
+                    })
+
+                var t1 = performance.now()
+                console.log("Call to getIndices() took " + (t1 - t0) + " milliseconds.")
+                this.countReviews()
+                this.countLessons()
+                return 0
+                })
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+                return -1
+            })
+        })
+        .catch(function(error) {
+            console.log("Error getting documents: ", error);
+            return -1
+        })
+    }
+
+    componentDidMount(){
+        this.getIndices();
     }
 
     render() {
@@ -24,7 +107,7 @@ class Home extends React.Component {
                 </ThemeProvider>
                 <div className="Home" style={{maxWidth: "80%", marginLeft: "10%", marginTop: "10%"}}>
                     <ThemeProvider theme={this.props.theme}>
-                        
+                        У тебя {this.state.lessonQueue ? this.state.lessonQueue.length: null} уроков
                     </ThemeProvider>
                 </div>
             </div>
